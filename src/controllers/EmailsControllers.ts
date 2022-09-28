2b1a57c04d65104a803100cce86f38024d90baa3
MarcoAGreco <marcoandreagreco@gmail.com>
2022-09-28 17:02:26 +0200

    [CHK-643] fix: lint

--- src/controllers/EmailsControllers.ts
+++ src/controllers/EmailsControllers.ts
@@ -178,7 +178,7 @@ export const sendEmail = async (
                     pdfData,
                     "test.pdf"
                   )
-                ),
+                )
             )
           );
         } catch (e) {
