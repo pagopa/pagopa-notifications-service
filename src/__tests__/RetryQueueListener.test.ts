3834fbffd30f21669453081613651bf0c349c09e
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.station>
2022-11-28 09:37:52 +0100

    fix retry queue listener tet

--- src/__tests__/RetryQueueListener.test.ts
+++ src/__tests__/RetryQueueListener.test.ts
@@ -6,7 +6,7 @@ import { Browser } from "puppeteer";
 import { Envelope } from "nodemailer/lib/mime-node";
 import * as SESTransport from "nodemailer/lib/ses-transport";
 import { Transporter } from "nodemailer";
-import { addRetryQueueListener, removeListener } from "../queues/RetryQueueListener";
+import { addRetryQueueListener } from "../queues/RetryQueueListener";
 import { QueueClient, QueueReceiveMessageResponse } from "@azure/storage-queue";
 import * as puppeteer from "puppeteer";
 import * as registerHelpers from "handlebars-helpers";
