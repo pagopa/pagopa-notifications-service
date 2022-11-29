659af9f39300d8332e99e099c9d2cb68cfb19579
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.station>
2022-11-29 19:27:31 +0100

    fix test config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules"],
+  testPathIgnorePatterns: ["dist","/node_modules"],
   testMatch: [
     "**/__tests__/*.ts"
   ],
