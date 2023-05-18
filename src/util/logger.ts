84586b19b4f6291000d0b8716695fe47ff0f9726
Simone Infante <simone.infante@pagopa.it>
2023-05-18 19:47:15 +0200

    feat: add file logs

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,6 +4,9 @@ import ecsFormat = require("@elastic/ecs-winston-format");
 export const logger = createLogger({
   format: ecsFormat(),
   transports: [
-    new transports.Console({ handleExceptions: true, handleRejections: true })
+    new transports.Console({ handleExceptions: true, handleRejections: true }),
+    new transports.File({
+      filename: "/var/log/containers/logs.log"
+    })
   ]
 });
