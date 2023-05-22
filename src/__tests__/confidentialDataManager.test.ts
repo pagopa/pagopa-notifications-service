08fd267f4e3220e5306581d4f8f7297beee4ca81
Fernando-Granato <fernando.granato@pagopa.it>
2023-05-22 11:01:07 +0200

    fix: tests

--- src/__tests__/confidentialDataManager.test.ts
+++ src/__tests__/confidentialDataManager.test.ts
@@ -2,6 +2,7 @@ import { pipe } from "fp-ts/lib/function";
 import { mockReq } from "../__mocks__/data_mock";
 import { apiPdvClient,decryptBody,encryptBody} from "../util/confidentialDataManager";
 import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
 
 describe("confidential data manager tests", () => {
     const token = "2784acb0-56e3-494b-93cb-0f8ecf3b0b8d"
@@ -16,68 +17,38 @@ describe("confidential data manager tests", () => {
       } as any;
       
 it("decrypt OK from PDV", async () => {
-    jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue({_tag: "Right", right:{ status:200 , value: {pii: JSON.stringify(requestMock.body)}, headers: "" as any} });
-    const result = await pipe(
-        decryptBody(token),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(true);
+   jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue({_tag: "Right", right:{ status:200 , value: {pii: JSON.stringify(requestMock.body)}, headers: "" as any} });
+   const result = await decryptBody(token)();
+   expect(E.isRight(result)).toBe(true);
 })
 
 it("decrypt KO from PDV", async () => {
-    jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue({_tag:"Left", left: "" as any });
-    const result = await pipe(
-        decryptBody(token),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(false);
+    jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue(E.left([]));
+    const result = await decryptBody(token)();
+    expect(E.isRight(result)).toBe(false);
 })
 
 it("decrypt OK from PDV with ko http status", async () => {
     jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue({_tag: "Right", right:{ status: 403 , value: undefined, headers: "" as any} });
-    const result = await pipe(
-        decryptBody(token),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(false);
+    const result = await decryptBody(token)();
+    expect(E.isRight(result)).toBe(false);
 })
 
 it("encrypt OK from PDV", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue({_tag: "Right", right:{ status: 200 , value: {token: token}, headers: "" as any} });
-    const result = await pipe(
-        encryptBody(requestMock.body),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(true);
+    const result = await encryptBody(requestMock.body)();
+    expect(E.isRight(result)).toBe(true);
 })
 
 it("encrypt KO from PDV", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue({_tag:"Left", left: "" as any });
-    const result = await pipe(
-        encryptBody(requestMock.body),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(false);
+    const result = await encryptBody(requestMock.body)();
+    expect(E.isRight(result)).toBe(false);
 })
 
 it("encrypt OK from PDV with ko http status", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue({_tag: "Right", right:{ status: 403 , value: undefined, headers: "" as any} });
-    const result = await pipe(
-        encryptBody(requestMock.body),
-        TE.map(() => true),
-        TE.mapLeft(() => false),
-        TE.toUnion
-    )()
-   expect(result).toBe(false);
+    const result = await encryptBody(requestMock.body)();
+    expect(E.isRight(result)).toBe(false);
 })
 });
