e0de5fca270f28686d025249cbe19292640999c8
Fernando-Granato <fernando.granato@pagopa.it>
2023-05-22 16:05:57 +0200

    fix: cast for NotificationRequest body

--- src/__tests__/confidentialDataManager.test.ts
+++ src/__tests__/confidentialDataManager.test.ts
@@ -1,17 +1,17 @@
-import { pipe } from "fp-ts/lib/function";
 import { mockReq } from "../__mocks__/data_mock";
 import { apiPdvClient,decryptBody,encryptBody} from "../util/confidentialDataManager";
-import * as TE from "fp-ts/lib/TaskEither";
 import * as E from "fp-ts/lib/Either";
+import { NotificationEmailRequest } from "../generated/definitions/NotificationEmailRequest";
+import { EmailString } from "@pagopa/ts-commons/lib/strings";
 
 describe("confidential data manager tests", () => {
     const token = "2784acb0-56e3-494b-93cb-0f8ecf3b0b8d"
     const body = {
-        to: "error@email.it",
+        to: "error@email.it" as EmailString,
         subject: "subjectTest",
         templateId: "success",
         parameters: mockReq
-    } as any;
+    } as NotificationEmailRequest;
       
 it("decrypt OK from PDV", async () => {
    jest.spyOn(apiPdvClient, 'findPiiUsingGET').mockResolvedValue(E.right({ status:200 , value: {pii: JSON.stringify(body)}, headers: "" as any}));
@@ -33,19 +33,19 @@ it("decrypt OK from PDV with ko http status", async () => {
 
 it("encrypt OK from PDV", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue(E.right({ status:200 , value: {token: token} , headers: "" as any}));
-    const result = await encryptBody(body)();
+    const result = await encryptBody(JSON.stringify(body))();
     expect(E.isRight(result)).toBe(true);
 })
 
 it("encrypt KO from PDV", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue(E.left([]));
-    const result = await encryptBody(body)();
+    const result = await encryptBody(JSON.stringify(body))();
     expect(E.isRight(result)).toBe(false);
 })
 
 it("encrypt OK from PDV with ko http status", async () => {
     jest.spyOn(apiPdvClient, 'saveUsingPUT').mockResolvedValue(E.right({ status: 403 , value: undefined, headers: "" as any}));
-    const result = await encryptBody(body)();
+    const result = await encryptBody(JSON.stringify(body))();
     expect(E.isRight(result)).toBe(false);
 })
 });
