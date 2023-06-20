d8ef264ef8574093c97e742e8f1aedc4333398ba
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-06-20 14:53:28 +0200

    fix tests

--- src/controllers/__tests__/HealthControllers.test.ts
+++ src/controllers/__tests__/HealthControllers.test.ts
@@ -35,7 +35,7 @@ describe("health check", () => {
     MAX_RETRY_ATTEMPTS: 3,
     PORT: 3240,
     RETRY_QUEUE_NAME: "retry q name",
-    STORAGE_TRANSIENT_CONNECTION_STRING: "storagetransientconnection"
+    STORAGE_TRANSIENT_CONNECTION_STRING: "storagetransientconnection",
     STORAGE_DEADLETTER_CONNECTION_STRING: "storagedeadletterconnection"
   } as IConfig;
 
