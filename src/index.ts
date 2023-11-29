fc3ef8dabfbdd595a3cf88afa36349b480569987
Giovanni Berti <giovanni.berti@pagopa.it>
2023-11-29 15:15:16 +0100

    fix(debug): add logging hooks for uncaught exceptions

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ import { logger } from "./util/logger";
 // Retrieve server configuration
 const config = getConfigOrThrow();
 
+process.on("unhandledRejection", (reason, _promise) => {
+  logger.error(reason);
+});
+
+process.on("uncaughtException", reason => {
+  logger.error(reason);
+});
+
 // Define and start server
 app.startApp(config, logger).catch(error => {
   logger.error(`Error occurred starting server: ${error}`);
