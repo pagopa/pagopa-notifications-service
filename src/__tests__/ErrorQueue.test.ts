f9264ca493c29cbf7cff74a495b32bd47c334a37
Pietro Tota <pietro.tota@pagopa.it>
2023-11-29 13:25:22 +0100

    fix: add jest tests

--- src/__tests__/ErrorQueue.test.ts
+++ src/__tests__/ErrorQueue.test.ts
@@ -1,13 +1,38 @@
 import { sendMessageToErrorQueue } from "../queues/ErrorQueue"
 import { errorQueueClient } from "../util/queues";
-
+import apm = require("elastic-apm-node");
 describe("error queue", () => {
     
-    it("sendMessageToErrorQueue", () => {
+    beforeEach(async () => {
+        jest.resetAllMocks();
+    });
+
+    it("Message written to dead letter queue writing span", () => {
         jest.useFakeTimers();
-        const spySendMessages = jest.spyOn(errorQueueClient,'sendMessage');
-        sendMessageToErrorQueue("clientId","bodyEncryted");
-        expect(spySendMessages).toBeCalled();
+        const spySendMessages = jest.spyOn(errorQueueClient, 'sendMessage');
+        const transaction = apm.startTransaction("test transaction");
+        const span = transaction!.startSpan("test span");
+        const addLabelsSpanSpy = jest.spyOn(span!, "addLabels");
+        const endSpanSpy = jest.spyOn(span!, "end");
+        const apmStartSpanSpy = jest.spyOn(apm, 'startSpan').mockReturnValue(span);
+        sendMessageToErrorQueue("clientId", "bodyEncryted");
+        expect(apmStartSpanSpy).toBeCalledTimes(1);
+        expect(addLabelsSpanSpy).toBeCalledWith({
+            "deadLetterEvent.category": "RETRY_EVENT_NO_ATTEMPTS_LEFT",
+            "deadLetterEvent.serviceName": "pagopa-notifications-service"
+        });
+        expect(endSpanSpy).toBeCalledTimes(1);
+        expect(spySendMessages).toBeCalledTimes(1);
+        jest.useRealTimers();
+    });
+
+    it("Message written to dead letter queue when returned span is null", () => {
+        jest.useFakeTimers();
+        const spySendMessages = jest.spyOn(errorQueueClient, 'sendMessage');
+        const apmStartSpanSpy = jest.spyOn(apm, 'startSpan').mockReturnValue(null);
+        sendMessageToErrorQueue("clientId", "bodyEncryted");
+        expect(apmStartSpanSpy).toBeCalledTimes(1);
+        expect(spySendMessages).toBeCalledTimes(1);
         jest.useRealTimers();
     });
 });
