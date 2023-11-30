2d9c69ef06c3f538f6ee3dcb3046924759b8848d
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-11-30 17:45:45 +0100

    feat: add apm span for trace dead letter event (#72)
    * feat: add apm span for trace dead letter event
    
    * fix: add jest tests
    
    * fix: add create APM transaction
    
    * fix(debug): add logging hooks for uncaught exceptions
    
    * fix: add transaction close statement
    
    * fix: increase pod memory
    
    ---------
    
    Co-authored-by: Giovanni Berti <giovanni.berti@pagopa.it>

--- src/__tests__/ErrorQueue.test.ts
+++ src/__tests__/ErrorQueue.test.ts
@@ -1,13 +1,42 @@
 import { sendMessageToErrorQueue } from "../queues/ErrorQueue"
 import { errorQueueClient } from "../util/queues";
-
+import apm = require("elastic-apm-node");
 describe("error queue", () => {
-    
-    it("sendMessageToErrorQueue", () => {
+
+    beforeEach(async () => {
+        jest.resetAllMocks();
+    });
+
+    it("Message written to dead letter queue writing span", () => {
         jest.useFakeTimers();
-        const spySendMessages = jest.spyOn(errorQueueClient,'sendMessage');
-        sendMessageToErrorQueue("clientId","bodyEncryted");
-        expect(spySendMessages).toBeCalled();
+        const spySendMessages = jest.spyOn(errorQueueClient, 'sendMessage');
+        const transaction = apm.startTransaction("test transaction");
+        const span = transaction!.startSpan("test span");
+        const addLabelsSpanSpy = jest.spyOn(span!, "addLabels");
+        const endSpanSpy = jest.spyOn(span!, "end");
+        const endTransactionSpy = jest.spyOn(transaction!, "end");
+        const apmStartTransactionSpy = jest.spyOn(apm, 'startTransaction').mockReturnValue(transaction);
+        const transactionStartSpanSpy = jest.spyOn(transaction!, 'startSpan').mockReturnValue(span);
+        sendMessageToErrorQueue("clientId", "bodyEncryted");
+        expect(apmStartTransactionSpy).toBeCalledTimes(1);
+        expect(transactionStartSpanSpy).toBeCalledTimes(1);
+        expect(addLabelsSpanSpy).toBeCalledWith({
+            "deadLetterEvent_category": "RETRY_EVENT_NO_ATTEMPTS_LEFT",
+            "deadLetterEvent_serviceName": "pagopa-notifications-service"
+          });
+        expect(endSpanSpy).toBeCalledTimes(1);
+        expect(endTransactionSpy).toBeCalledTimes(1);
+        expect(spySendMessages).toBeCalledTimes(1);
+        jest.useRealTimers();
+    });
+
+    it("Message written to dead letter queue when returned span is null", () => {
+        jest.useFakeTimers();
+        const spySendMessages = jest.spyOn(errorQueueClient, 'sendMessage');
+        const apmStartSpanSpy = jest.spyOn(apm, 'startTransaction').mockReturnValue(null);
+        sendMessageToErrorQueue("clientId", "bodyEncryted");
+        expect(apmStartSpanSpy).toBeCalledTimes(1);
+        expect(spySendMessages).toBeCalledTimes(1);
         jest.useRealTimers();
     });
 });
--- src/queues/ErrorQueue.ts
+++ src/queues/ErrorQueue.ts
@@ -1,13 +1,43 @@
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
+import apm = require("elastic-apm-node");
 import { errorQueueClient } from "../util/queues";
 
+const deadLetterErrorLabels: apm.Labels = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  deadLetterEvent_category: "RETRY_EVENT_NO_ATTEMPTS_LEFT",
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  deadLetterEvent_serviceName: "pagopa-notifications-service"
+};
 export const sendMessageToErrorQueue = async (
   bodyEncrypted: string,
   clientId: string
 ): Promise<void> => {
-  await errorQueueClient.sendMessage(
+  const sendDeadLetterQueueMessage = errorQueueClient.sendMessage(
     JSON.stringify({
       bodyEncrypted,
       clientId
     })
+  );
+  await pipe(
+    O.fromNullable(
+      apm.startTransaction("No attempts left for retry user mail send")
+    ),
+    O.map(transaction => {
+      const span = transaction.startSpan("Writing event to dead letter queue");
+      return { span, transaction };
+    }),
+    O.map(({ span, transaction }) => {
+      span?.addLabels(deadLetterErrorLabels);
+      return { span, transaction };
+    }),
+    O.fold(
+      async () => sendDeadLetterQueueMessage,
+      async ({ span, transaction }) => {
+        span?.end();
+        transaction?.end();
+        return sendDeadLetterQueueMessage;
+      }
+    )
   );
 };
