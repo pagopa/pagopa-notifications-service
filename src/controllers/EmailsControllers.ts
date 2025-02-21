045ad00047de014c815312667710eea0e7d18bcb
EmanueleBVtech <emanuele.serrao@bvtech.com>
2025-02-21 12:50:14 +0100

    Update EmailsControllers.ts

--- src/controllers/EmailsControllers.ts
+++ src/controllers/EmailsControllers.ts
@@ -41,8 +41,6 @@ import { retryQueueClient } from "../util/queues";
 import { sendMessageToErrorQueue } from "../queues/ErrorQueue";
 import { encryptBody } from "../util/confidentialDataManager";
 
-var singletonPage: any = null;
-
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const sendEmailWithAWS = async (
   senderEmail: string,
