376135052ddc5652277f49f81303fd5905061cd1
EmanueleBVtech <emanuele.serrao@bvtech.com>
2025-02-24 14:32:59 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+/**
+ * Create and Run the server
+ */
 import "elastic-apm-node/start";
-import cluster from "cluster";
-import os from "os";
 import * as app from "./app";
 import { getConfigOrThrow } from "./util/config";
 import { logger } from "./util/logger";
@@ -16,22 +17,7 @@ process.on("uncaughtException", reason => {
   logger.error(reason);
 });
 
-if (cluster.isMaster) {
-  const cpus = os.cpus();
-  logger.info(`Master process is running. Forking ${cpus.length} workers...`);
-
-  // Fork workers
-  cpus.forEach(() => {
-    cluster.fork();
-  });
-
-  cluster.on("exit", worker => {
-    logger.warn(`Worker ${worker.process.pid} died. Forking a new worker...`);
-    cluster.fork();
-  });
-} else {
-  // Define and start server
-  app.startApp(config, logger).catch(error => {
-    logger.error(`Error occurred starting server: ${error}`);
-  });
-}
+// Define and start server
+app.startApp(config, logger).catch(error => {
+  logger.error(`Error occurred starting server: ${error}`);
+});
