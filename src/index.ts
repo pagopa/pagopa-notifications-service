39ab3538d818db83ddaa0a09557446dc00de1007
Pietro Tota <pietro.tota@pagopa.it>
2025-03-05 18:25:45 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 /**
  * Create and Run the server
  */
-import * as opentelemetryInstrumentation from "./opentelemetryInstrumentation";
 import * as app from "./app";
 import { getConfigOrThrow } from "./util/config";
 import { logger } from "./util/logger";
-
-logger.info(`Starting OTEL SDK`);
-opentelemetryInstrumentation.opentelemetrySdk.start();
-logger.info(`OTEL SDK started`);
 
 // Retrieve server configuration
 const config = getConfigOrThrow();
@@ -26,11 +21,3 @@ app.startApp(config, logger).catch(error => {
   logger.error(`Error occurred starting server: ${error}`);
 });
 
-process.on("SIGTMER", () => {
-  logger.info(`Stopping OTEL SDK`);
-  opentelemetryInstrumentation.opentelemetrySdk
-    .shutdown()
-    .then(() => logger.info("SDK terminated"))
-    .catch(error => logger.error(`Error while stopping OTEL SDK: ${error}`))
-    .finally(() => process.exit(0));
-});
