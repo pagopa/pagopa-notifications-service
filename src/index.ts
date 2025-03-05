f481b3b522dc69b4385ac19dbb1ce27c38a62048
Pietro Tota <pietro.tota@pagopa.it>
2025-03-05 17:09:33 +0100

    wip: OTEL instrumentation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 /**
  * Create and Run the server
  */
-import "elastic-apm-node/start";
+import * as opentelemetryInstrumentation from "./opentelemetryInstrumentation";
 import * as app from "./app";
 import { getConfigOrThrow } from "./util/config";
 import { logger } from "./util/logger";
+
+logger.info(`Starting OTEL SDK`);
+opentelemetryInstrumentation.opentelemetrySdk.start();
+logger.info(`OTEL SDK started`);
 
 // Retrieve server configuration
 const config = getConfigOrThrow();
@@ -20,4 +24,13 @@ process.on("uncaughtException", reason => {
 // Define and start server
 app.startApp(config, logger).catch(error => {
   logger.error(`Error occurred starting server: ${error}`);
+});
+
+process.on("SIGTMER", () => {
+  logger.info(`Stopping OTEL SDK`);
+  opentelemetryInstrumentation.opentelemetrySdk
+    .shutdown()
+    .then(() => logger.info("SDK terminated"))
+    .catch(error => logger.error(`Error while stopping OTEL SDK: ${error}`))
+    .finally(() => process.exit(0));
 });
