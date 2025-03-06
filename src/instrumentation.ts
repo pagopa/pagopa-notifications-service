d886acfa4954c8cc50a0b02e4c1ddd15b3ae1271
Pietro Tota <pietro.tota@pagopa.it>
2025-03-06 14:31:25 +0100

    fix: add metrics instrumentation for node runtime

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,6 +4,8 @@ import { OTLPTraceExporter as OTLPGrpcTraceExporter } from "@opentelemetry/expor
 import { Resource } from "@opentelemetry/resources";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 import { OTLPMetricExporter as OTLPGrpcMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
+import { RuntimeNodeInstrumentation } from "@opentelemetry/instrumentation-runtime-node";
+
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import packageJson from "../package.json";
 const appVersion = packageJson.version;
@@ -13,16 +15,25 @@ const resource = Resource.default().merge(
     [ATTR_SERVICE_VERSION]: `${appVersion}`
   })
 );
+const metricReader = new PeriodicExportingMetricReader({
+  exportIntervalMillis: 60000,
+  exportTimeoutMillis: 30000,
+  exporter: new OTLPGrpcMetricExporter()
+});
 
 const sdk = new NodeSDK({
-  instrumentations: [getNodeAutoInstrumentations()],
-  metricReader: new PeriodicExportingMetricReader({
-    exportIntervalMillis: 60000,
-    exportTimeoutMillis: 30000,
-    exporter: new OTLPGrpcMetricExporter()
-  }),
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-express": {
+        enabled: true
+      }
+    }),
+    new RuntimeNodeInstrumentation({
+      enabled: true
+    })
+  ],
+  metricReader,
   resource,
   traceExporter: new OTLPGrpcTraceExporter()
 });
-
 sdk.start();
