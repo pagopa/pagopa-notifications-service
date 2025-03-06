a2a5bb71483ae66d29b2938788719bf0b2f68b5c
Pietro Tota <pietro.tota@pagopa.it>
2025-03-06 11:45:51 +0100

    fix: test metric exporter

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,11 +1,11 @@
-/*import { NodeSDK } from "@opentelemetry/sdk-node";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
+import { OTLPTraceExporter as OTLPGrpcTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { Resource } from "@opentelemetry/resources";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
-import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
+import { OTLPMetricExporter as OTLPGrpcMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import packageJson from "../package.json";
-
 const appVersion = packageJson.version;
 
 const resource = Resource.default().merge(
@@ -16,9 +16,13 @@ const resource = Resource.default().merge(
 
 const sdk = new NodeSDK({
   instrumentations: [getNodeAutoInstrumentations()],
-  metricReader: new PrometheusExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exportIntervalMillis: 60000,
+    exportTimeoutMillis: 30000,
+    exporter: new OTLPGrpcMetricExporter()
+  }),
   resource,
-  traceExporter: new OTLPTraceExporter()
+  traceExporter: new OTLPGrpcTraceExporter()
 });
 
-sdk.start();*/
+sdk.start();
