0d40e25f7aa38e65e8955f92248ff30a7ed99818
Pietro Tota <pietro.tota@pagopa.it>
2025-03-07 11:36:27 +0100

    wip: attempt auto instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,15 +1,11 @@
-import { NodeSDK } from "@opentelemetry/sdk-node";
+/*import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter as OTLPGrpcTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 import { Resource } from "@opentelemetry/resources";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 import { OTLPMetricExporter as OTLPGrpcMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
 import { RuntimeNodeInstrumentation } from "@opentelemetry/instrumentation-runtime-node";
-import { HostMetrics } from "@opentelemetry/host-metrics";
-import {
-  MeterProvider,
-  PeriodicExportingMetricReader
-} from "@opentelemetry/sdk-metrics";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import packageJson from "../package.json";
 
 const appVersion = packageJson.version;
@@ -22,15 +18,9 @@ const resource = Resource.default().merge(
 const metricReader = new PeriodicExportingMetricReader({
   exportIntervalMillis: 60000,
   exportTimeoutMillis: 30000,
-  exporter: new OTLPGrpcMetricExporter()
+  exporter: new OTLPGrpcMetricExporter(),
+  metricProducers: []
 });
-
-const meterProvider = new MeterProvider({
-  readers: [metricReader]
-});
-
-const hostMetrics = new HostMetrics({ meterProvider });
-hostMetrics.start();
 
 const sdk = new NodeSDK({
   instrumentations: [
@@ -44,7 +34,9 @@ const sdk = new NodeSDK({
       monitoringPrecision: 5000 // 5 seconds
     })
   ],
+  metricReader,
   resource,
   traceExporter: new OTLPGrpcTraceExporter()
 });
 sdk.start();
+*/
